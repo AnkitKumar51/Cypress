ea50c544cb805ac2c72dde0981c84ec282957c82
ankit5184 <Anki@t5184>
2023-05-31 14:37:48 +0530

    update

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
-  projectId: 'ihjenn',
+  projectId: 'jx2v6s',
   e2e: {
     "baseUrl": "https://www.saucedemo.com",
     "chromeWebSecurity": false,
